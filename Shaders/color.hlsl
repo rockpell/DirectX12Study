8dd5a35c99c005550ebaad9713edd76b3b3814d7
rockpell <aythffk@gmail.com>
2019-08-12 23:43:52 +0900

    정점 왜곡 셰이더 주석 처리

--- Shaders/color.hlsl
+++ Shaders/color.hlsl
@@ -26,8 +26,8 @@ VertexOut VS(VertexIn vin)
 {
 	VertexOut vout;
 	
-	vin.PosL.xy += 0.5f*sin(vin.PosL.x)*sin(3.0f*gTime);
-	vin.PosL.z *= 0.6f + 0.4f*sin(2.0f*gTime);
+	/*vin.PosL.xy += 0.5f*sin(vin.PosL.x)*sin(3.0f*gTime);
+	vin.PosL.z *= 0.6f + 0.4f*sin(2.0f*gTime);*/
 
 	// Transform to homogeneous clip space.
 	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
