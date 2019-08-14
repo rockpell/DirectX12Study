2ee29f9c727ca6f52a0e7ff901bbf13459531918
rockpell <aythffk@gmail.com>
2019-08-14 19:54:56 +0900

    7장 예제 소스 파일 추가

--- Shaders/color.hlsl
+++ Shaders/color.hlsl
@@ -4,44 +4,51 @@
 // Transforms and colors geometry.
 //***************************************************************************************
 
-cbuffer cbPerObject : register(b0)
-{
-	float4x4 gWorldViewProj; 
-	float gTime;
+cbuffer cbPerObject : register(b0) {
+	float4x4 gWorld;
 };
 
-struct VertexIn
-{
+cbuffer cbPass : register(b1) {
+	float4x4 gView;
+	float4x4 gInvView;
+	float4x4 gProj;
+	float4x4 gInvProj;
+	float4x4 gViewProj;
+	float4x4 gInvViewProj;
+	float3 gEyePosW;
+	float cbPerObjectPad1;
+	float2 gRenderTargetSize;
+	float2 gInvRenderTargetSize;
+	float gNearZ;
+	float gFarZ;
+	float gTotalTime;
+	float gDeltaTime;
+};
+
+struct VertexIn {
 	float3 PosL  : POSITION;
-    float4 Color : COLOR;
+	float4 Color : COLOR;
 };
 
-struct VertexOut
-{
+struct VertexOut {
 	float4 PosH  : SV_POSITION;
-    float4 Color : COLOR;
+	float4 Color : COLOR;
 };
 
-VertexOut VS(VertexIn vin)
-{
+VertexOut VS(VertexIn vin) {
 	VertexOut vout;
-	
-	/*vin.PosL.xy += 0.5f*sin(vin.PosL.x)*sin(3.0f*gTime);
-	vin.PosL.z *= 0.6f + 0.4f*sin(2.0f*gTime);*/
 
 	// Transform to homogeneous clip space.
-	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
-	
+	float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+	vout.PosH = mul(posW, gViewProj);
+
 	// Just pass vertex color into the pixel shader.
-    vout.Color = vin.Color;
-    
-    return vout;
+	vout.Color = vin.Color;
+
+	return vout;
 }
 
 float4 PS(VertexOut pin) : SV_Target
 {
-	clip(pin.Color.r - 0.5f);
-    return pin.Color;
+	return pin.Color;
 }
-
-
